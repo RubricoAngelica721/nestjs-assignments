7e7150829215c97489669accc920fd667a65b0df
Ma. Angelica M. Rubrico <87623792+RubricoAngelica721@users.noreply.github.com>
2024-11-09 14:55:11 +0800

    Completed Assignment 2: Prime Number

--- src/assignments/assignments.controller.ts
+++ src/assignments/assignments.controller.ts
@@ -2,15 +2,13 @@ import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('assignments')
 export class AssignmentController {
-  @Get('fibonacci/:n')
-  fibonacci(@Param('n') n: number) {
-    const sequence = [];
-    let a = 0, e = 1;
-    for (let i = 0; i < n; i++) {
-      sequence.push(a);
-      [a, e] = [e, a + e];
+  
+  @Get('prime/:number')
+  isPrime(@Param('number') number: number) {
+    if (number < 2) return { isPrime: false };
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+      if (number % i === 0) return { isPrime: false };
     }
-    
-    return { sequence };
+    return { isPrime: true };
   }
 }
